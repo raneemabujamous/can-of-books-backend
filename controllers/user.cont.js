3f9bfbe23e541fdfb002a8721c97e47f6dc831dd
raneemabujamous <raneemabujamous1998@gmail.com>
2021-09-21 00:15:19 +0300

    Added

--- controllers/user.cont.js
+++ controllers/user.cont.js
@@ -1,13 +1,51 @@
-const {usermodel ,seedfunction}=require("../module/User");
-let usercontroolers= (req,res)=>{
-    // seedfunction()
-let bookId = req.query.id
-    usermodel.findOne({_id:bookId}).then(data=>{
-         res.json(data);
-     })  
+const { usermodel, seedfunction } = require("../module/User");
 
+
+let usercontroolers = (req, res) => {
+
+    usermodel.find().then(data => {
+        res.json(data);
+    })
+}
+
+const createBook = async (req, res) => {
+    // console.log(req.body)
+    const email= req.body.email;
+    const title = req.body.title;
+    const description = req.body.description;
+    console.log(description);
+    const status = req.body.status;
+
+    const newObj = new usermodel({
+
+        email: email,
+        title: title,
+        description:description,
+        status: status
+
+    })
+    console.log(newObj, "hi from user");
+    newObj.save();
+    res.json(newObj);
 }
 
 
-module.exports=  usercontroolers
+const deleteBook = (req,res) => {
+    try{
+        const id=req.params.id;
 
+      
+            usermodel.deleteOne({ _id: book },(error,book)=>{
+                res.json(book);
+    });
+    }
+    catch(error){
+        res.send(error.info);
+    }
+    };
+module.exports = {
+    usercontroolers,
+    createBook,
+    deleteBook,
+
+}
--- server.js
+++ server.js
@@ -9,18 +9,20 @@ const axios = require('axios')
 const PORT = process.env.PORT;
 const mongoose = require('mongoose');
 const MONGO_DB_URL = process.env.MONGO_DB_URL
-const usercontroolers = require('./controllers/user.cont')
-const seedfunction = require('./module/User')
+const {usercontroolers ,createBook ,deleteBook} = require('./controllers/user.cont')
+// const {seedfunction} = require('./module/User')
 
 mongoose.connect(`${MONGO_DB_URL}/book`, { useNewUrlParser: true, useUnifiedTopology: true });
+app.use(express.json());
 
-app.get('/app' , (req,res)=>{
-    seedfunction()
-    res.json("good")
-} )
+// app.get('/app' , (req,res)=>{
+//     seedfunction()
+//     console.log(seedfunction())
+//     res.json("good")
+// } )
 app.get('/book',usercontroolers);
-
-
+app.post('/create-book' , createBook)
+app.delete('/delete-book/:id' , deleteBook)
 app.listen(PORT,()=>{
     console.log(`Listening to port ${PORT}`);
 })
